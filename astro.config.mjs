e9815ea272ce315e72d2144a6b69001677dc0c27
Jesús Sanz <jesussanz77@gmail.com>
2025-08-06 17:37:18 +0200

    feat(fix): www

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.jesussanz.dev/',
+  site: 'https://jesussanz.dev/',
   vite: {
     plugins: [tailwindcss()],
   },
